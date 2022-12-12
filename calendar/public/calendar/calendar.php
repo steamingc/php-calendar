9842da58c0054488341fa134b3a0edf75ecc1fdb
steamingc <steamingc@gmail.com>
2022-12-12 15:50:46 +0900

    221212 만능형 기능 추가 및 스타일 수정

--- calendar/public/calendar/calendar.php
+++ calendar/public/calendar/calendar.php
@@ -1,155 +1,234 @@
+<?php
+//---- 오늘 날짜
+$thisyear = date('Y'); // 4자리 연도
+$thismonth = date('n'); // 0을 포함하지 않는 월
+$today = date('j'); // 0을 포함하지 않는 일
+
+//---- $year, $month 값이 없으면 현재 날짜
+$year = isset($_GET['year']) ? $_GET['year'] : $thisyear;
+$month = isset($_GET['month']) ? $_GET['month'] : $thismonth;
+$day = isset($_GET['day']) ? $_GET['day'] : $today;
+
+//---- 지난 달, 다음 달, 작년, 내년
+$prev_month = $month - 1;
+$next_month = $month + 1;
+$prev_year = $next_year = $year;
+
+//---- 해당 달이 1월 혹은 12월이면 월, 연도 변경해줌
+if ($month == 1) {
+    $prev_month = 12;
+    $prev_year = $year - 1;
+} else if ($month == 12) {
+    $next_month = 1;
+    $next_year = $year + 1;
+}
+
+//---- 작년, 내년 버튼
+$preyear = $year - 1;
+$nextyear = $year + 1;
+
+//---- 어제, 내일 버튼
+$predate = date("Y-m-d", mktime(0, 0, 0, $month - 1, 1, $year));
+$nextdate = date("Y-m-d", mktime(0, 0, 0, $month + 1, 1, $year));
+
+// 1. 총일수 구하기
+$max_day = date('t', mktime(0, 0, 0, $month, 1, $year)); // 해당월의 마지막 날짜
+//echo '총요일수'.$max_day.'<br />';
+
+// 2. 시작요일 구하기
+$start_week = date("w", mktime(0, 0, 0, $month, 1, $year)); // 일요일 0, 토요일 6
+
+// 3. 총 몇 주인지 구하기
+$total_week = ceil(($max_day + $start_week) / 7);
+
+// 4. 마지막 요일 구하기
+$last_week = date('w', mktime(0, 0, 0, $month, $max_day, $year));
+?>
+
 <!DOCTYPE html>
 <html lang="ko">
 
-<head>
-    <meta charset = "utf-8">
-    <title>12월 달력 만들기</title>
+    <head>
+        <meta charset="utf-8">
+        <title>12월 달력 만들기</title>
 
-    <!-- style 태그의 type 속성은 style 요소의 미디어 타입을 명시함. text/css 속성값은 콘텐츠가 css임을 나타낸다 -->
-    <!-- css : 스타일, 레이아웃 등 사용자에게 문서를 표시하는 방법을 지정하는 언어 -->
-    <style type="text/css">
-        .table1 { 
-            border:1px solid white; 
-            /* width:720px;  */
-            background-color:#DBBE9F; 
-            border-collapse:collapse; 
-            text-align:center; 
-            color:white;
-        }
-        
-        tr, th, td {
-            border:1px solid white;
-            width:80px;
-            height:80px;
-        }
-
-        .sunday {
-            width:80px; 
-            height:80px;
-            color:#B75F5F;
-        }
-
-        .saturday {
-            width:80px; 
-            height:80px;
-            color:#6A8DAB;
-        }
-
-        .info {
-            background-color:#DBBE9F;
-            width:160px;
-            color:#6A8DAB;
-            border-collapse:collapse;
-        }
-
-        .year {
-            height:175px;
-            border:1px solid white;
-            text-align:center; 
-            vertical-align:middle;
-        }
-
-    </style>
-</head>
-
-<body>
-    <?php 
-        if( !isset($today) ) { $today = date("Y-m-d"); }  //today가 초기화 되어 있지 않으면 오늘 날짜로 설정 (date함수)
-        // echo "$today"."<br>";   //날짜 확인
-        // echo var_dump($today);  //변수 타입 확인
-
-
-        //함수 생성
-        // function cal($today) {
-            $today_arr = explode("-", $today);    //오늘 날짜의 연,월,일을 분리하여 저장
-            $today_arr_y = $today_arr[0];   //연
-            $today_arr_m = $today_arr[1];   //월
-            $today_arr_d = $today_arr[2];   //일
-
-            $this_month_tday = date("t", mktime(0,0,0,$today_arr_m, $today_arr_d, $today_arr_y)); //이번 달 월의 일수
-            $this_month_first = date("N", mktime(0,0,0,$today_arr_m, 1, $today_arr_y)); //첫날 요일 (N이 요일의 숫자 표현1~7)
-            $start_point = $this_month_first % 7;   //이번 달 1일이 언제 시작하는지 (1일 전 공백 숫자)
-
-            //달력 줄 개수
-            $line = ($this_month_tday + $start_point) / 7; //달력 모든 칸의 개수/7
-            $line = ceil($line);    //올림
-            $line = $line - 1;
-
-            // //지난 달
-            $p_m=date("Y-m-d", mktime(0,0,0,$today_arr_m-1,$today_arr_d,$today_arr_y));
-
-            // //다음 달
-            $n_m=date("Y-m-d", mktime(0,0,0,$today_arr_m+1,$today_arr_d,$today_arr_y));
-
-            echo "<div style=\"display:inline-flex; height:175px;\">";
-            echo "
-            <div>
-                <table class=\"info\">
-                    <tr><td class=\"year\">
-                        <h3 style=\"text-decoration:underline\">&nbsp&nbsp&nbsp".$today_arr_y."&nbsp&nbsp&nbsp</h3>
-                        <h1>".$today_arr_m."</h1>
-                    </td></tr>".
-                    //지난 달, 다음 달 넘어가는 여기 수정해야 함!! ㅜㅜㅜ
-                    "<tr><td class=\"year\"><h1><a href=<?php echo 'calendar.php?today=$p_m';>◀ ".($today_arr_m-1)."</a><h1></td></tr> 
-
-                    <tr><td class=\"year\"><h1>";
-                    if($today_arr_m<12) { 
-                        echo "$today_arr_m+1";
-                    } else if($today_arr_m==12){
-                        echo "1";
-                    }
-                    echo " ▶<h1></td></tr>
-                
-                </table>
-            </div>";
-    
-            echo "<div>";
-            echo ("
-                <table class=\"table1\">
-                    <tr>
-                        <th class=\"sunday\"><h1>S</h1></th>
-                        <th><h1>M</h1></th>
-                        <th><h1>T</h1></th>
-                        <th><h1>W</h1></th>
-                        <th><h1>T</h1></th>
-                        <th><h1>F</h1></th>
-                        <th class=\"saturday\"><h1>S</h1></th>
-                    </tr>
-            ");
-            
-            for ($i=0; $i<=$line; $i++) {   //달력 줄 수만큼 반복
-                echo "<tr>"; 
-
-                for ($j=1; $j<=7; $j++) {   //요일만큼 반복
-
-                        /* 칸에 번호를 매겨줌. 1일이 되기 전 공백들부터 마이너스 값으로 채움 */
-                        $iv = 7 * $i + $j;
-                        $iu = $iv - $line;  
-
-                        if($j==1) { //일요일 날짜 색상 다르게
-                            echo "<td class=\"sunday\"><h1>";
-                        } else if($j==7) {  //토요일
-                            echo "<td class=\"saturday\"><h1>";
-                        } else echo "<td><h1>";
-
-                        if($iu <= 0 || $iu > $this_month_tday) { 
-                            echo "&nbsp";
-                        } else {
-                            $today = date("Y-m-d", mktime(0,0,0,$today_arr_y, $iu, $today_arr_d));  //현재 칸의 날짜
-                            echo "$iu";
-                        }
-                        echo "</h1></td>";
-                    }
-                    echo "</tr>";
+        <!-- style 태그의 type 속성은 style 요소의 미디어 타입을 명시함. text/css 속성값은 콘텐츠가 css임을 나타낸다 -->
+        <!-- css : 스타일, 레이아웃 등 사용자에게 문서를 표시하는 방법을 지정하는 언어 -->
+        <style type="text/css">
+            .row {
+                width: 720px;
+                /* 디브 태그 간의 높이를 같게 한다 (폭도 같이 하고 싶으면 flex:1 추가하기) */
+                display: inline-flex; 
             }
-            echo "</table>";
-            echo "</div>";
-            echo "</div>";
-        // }
-    ?>
-<!-- 
-    <?php
-        echo cal(today);
-    ?> -->
-</body>
+
+            .table1 {
+                border: 1px solid white;
+                width: 560px; 
+                background-color: #DBBE9F;
+                border-collapse: collapse;
+                text-align: center;
+                color: white;
+            }
+
+            th, 
+            td {
+                border: 1px solid white;
+                width: 80px;
+                height: 80px;
+                vertical-align: middle;
+            }
+
+            a {
+                text-decoration:none;
+                color: #6A8DAB;
+            }
+
+            .sunday {
+                color: #B75F5F;
+                text-align: center;
+            }
+
+            .saturday {
+                color: #6A8DAB;
+                text-align: center;
+            }
+
+            .white {
+                color: white;
+                text-align: center;
+            }
+
+            .today {
+                color: #070707;
+                text-align: center;
+                font-weight: bold;
+                text-decoration: underline;
+                
+            }
+
+            .info {
+                background-color: #DBBE9F;
+                width: 160px;
+                color: #6A8DAB;
+                border-collapse: collapse;
+            }
+
+            .year {
+                height: 140px;
+                border: 1px solid white;
+                text-align: center;
+                vertical-align: middle;
+            }
+
+            
+        </style>
+    </head>
+
+    <body>
+
+        <div class="row">
+            <table class="info">
+                <tr>
+                    <td class="year">
+                        <h4 style="text-decoration:underline">&nbsp&nbsp&nbsp<?php echo $year ?>&nbsp&nbsp&nbsp</h4>
+                        <h1><?php echo $month ?></h1>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="year">
+                        <h2>
+                            <a href=<?php echo 'index.php?year='.$prev_year.'&month='.$prev_month.'&day=1'; ?>>◀ </a>
+                            <a href=<?php echo 'index.php?year='.$next_year.'&month='.$next_month.'&day=1'; ?>> ▶</a>
+                        </h2>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="year">
+                        <h2>
+                        <a href=<?php echo 'index.php?year='.$preyear.'&month='.$month.'&day=1'; ?>>◀◀ </a>
+                        <a href=<?php echo 'index.php?year='.$nextyear.'&month='.$month.'&day=1'; ?>> ▶▶</a>
+                        </h2>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="year">
+                        <h2>
+                            <a
+                                href=<?php echo 'index.php?year='.$thisyear.'&month='.$thismonth.'&day='.$today; ?>>
+                                오늘</a>
+                        </h2>
+                    </td>
+                </tr>
+
+            </table>
+            
+            <table class="table1">
+                <tr>
+                    <th class="sunday">
+                        <h1>S</h1>
+                    </th>
+                    <th>
+                        <h1>M</h1>
+                    </th>
+                    <th>
+                        <h1>T</h1>
+                    </th>
+                    <th>
+                        <h1>W</h1>
+                    </th>
+                    <th>
+                        <h1>T</h1>
+                    </th>
+                    <th>
+                        <h1>F</h1>
+                    </th>
+                    <th class="saturday">
+                        <h1>S</h1>
+                    </th>
+                </tr>
+
+            <?php
+            $day=1; //화면의 초기값을 1로 설정
+
+            //달력 줄 수만큼 반복
+            for ($i=1; $i<=$total_week; $i++) { 
+            ?>
+
+            <tr>
+
+            <?php
+            //총 가로칸 만들기. 요일만큼 반복
+            for ($j=0; $j<7; $j++) {
+                echo '<td><h1>';
+
+                //첫번째 주인데 시작 요일보다 $j가 작거나 마지막주인데 $j가 마지막 요일보다 크면 표시하지 않음
+                if(!(($i==1 && $j<$start_week) || ($i==$total_week && $j>$last_week))){
+                        //오늘이면 검은 색
+                    if($year==$thisyear && $month==$thismonth && $day==date("j")){
+                        $style = "today";
+                    } else if($j==0) {      //일요일
+                        $style = "sunday";
+                    } else if($j==6){       //토요일
+                        $style = "saturday";   
+                    } else {                //오늘이 아닌 다른 평일
+                        $style = "white";
+                    }
+
+                //날짜 출력
+                echo '<font class='.$style.'>';
+                echo $day;
+                echo '</font></h1>';
+
+                $day++;
+                }
+                echo "</td>";
+            }
+            echo "</tr>";
+            }
+            echo "</table></div>";
+        ?>
+    </body>
 </html>
